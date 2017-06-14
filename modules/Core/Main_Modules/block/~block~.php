8b511679effae9780cb277bf1599efc483af551d
SiT3D <sit3d@yandex.ru>
2017-06-14 13:21:08 +0300

    Update ~block~.php

--- modules/Core/Main_Modules/block/~block~.php
+++ modules/Core/Main_Modules/block/~block~.php
@@ -1,22 +1,30 @@
 <?php
 
+if ($dev_mode)
+{
     echo "\n";
-    if ($__cms_block_type !== 'empty')
-    {
-        echo '<' . $__cms_block_type . ' '; /* open tag */
-        echo_attrs($__cms_attrs);
-        echo '>';
-    }
+}
 
-    echo ($__text);
-    echo_modules($modules);
-    echo_modules($after_modules);
+if ($__cms_block_type !== 'empty')
+{
+    echo '<' . $__cms_block_type . ' ';
+    echo_attrs($__cms_attrs);
+    echo '>';
+}
 
-    if ($__cms_closing_type && $__cms_block_type !== 'empty')
-    {
-        echo '</' . $__cms_block_type . '>'; /* close tag */
-    }
+echo($__text);
+echo_modules($modules);
+echo_modules($after_modules);
+
+if ($__cms_closing_type && $__cms_block_type !== 'empty')
+{
+    echo '</' . $__cms_block_type . '>';
+}
+
+if ($dev_mode)
+{
     echo "\n";
+}
 
 
 
