fe7470f1d57b76c38bc88b56393761c0965ad275
SiT3D <sit3d@yandex.ru>
2017-06-14 13:52:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 define('HOME_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
 include_once HOME_PATH . '/modules/Core/globals.php';
-include_once HOME_PATH . '/modules/Core/CMS/logger/logger.php';
+
 
 $ttttttttttt = microtime_float();
 
@@ -20,14 +20,21 @@ foreach (\Modules::get_main_views() as $view)
 
 ////////////// лог посещаемости и скорости работы сайта. //////////////////
 
+$log_timer = microtime_float($ttttttttttt, 'time', false);
+$log_timer = round($log_timer, 3);
+
 if (!\MBCMS\routes::is_admin() && !\MBCMS\routes::is_ajax())
 {
-    $log_timer = microtime_float($ttttttttttt, 'time', false);
-    $log_timer = round($log_timer, 3);
-
     logger::write('people', [
         'browser_time_to_ms' => $log_timer * 1000,
         'page'               => $_SERVER['REQUEST_URI'],
         'ip'                 => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '',
     ]);
+
+    if (\MBCMS\configuration::factory()->is_dev_mod() && \trud\classes\auth::factory()->admin())
+    {
+        echo '<div style="z-index: 1000; position: fixed; background: #ffad4f; padding: 5px 20px; bottom: 0; left: 0; opacity: 0.5; pointer-events: none">
+    mods: ' . Module::get_modules_connect_count() . ' time: '. ($log_timer * 1000) . 'ms</div>';
+    }
 }
+
