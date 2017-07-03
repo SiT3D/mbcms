b7c6e4364c8d73dc4118f5834a6cfc34991ceeb8
SiT3D <sit3d@yandex.ru>
2017-07-03 16:41:02 +0300

    Update Site.php

--- modules/Core/Main_Modules/Site/Site.php
+++ modules/Core/Main_Modules/Site/Site.php
@@ -16,7 +16,111 @@ if (routes::is_admin() && !configuration::factory()->is_superadmin())
 class Site extends \Module
 {
 
-    public $json;
+    public static $TIMES  = [];
+    public static $TIMERS = [];
+    public        $json;
+
+    public static function START()
+    {
+        self::$TIMERS[] = microtime_float();
+    }
+
+    public static function END($key)
+    {
+        self::$TIMES[$key] = isset(self::$TIMES[$key]) ? self::$TIMES[$key] : 0;
+        self::$TIMES[$key] += microtime_float(array_pop(self::$TIMERS), 'time', false);
+    }
+
+    /**
+     * @param $log_timer
+     */
+    public static function site_info_panel($log_timer)
+    {
+        $log_timer = round($log_timer, 3);
+
+        if (!\MBCMS\routes::is_admin() && !\MBCMS\routes::is_ajax())
+        {
+            \logger::write('people', [
+                'browser_time_to_ms' => $log_timer * 1000,
+                'page'               => $_SERVER['REQUEST_URI'],
+                'ip'                 => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '',
+            ]);
+
+            if (\MBCMS\configuration::factory()->is_dev_mod() && \trud\classes\auth::factory()->admin())
+            {
+                $bdinfo   = \MBCMS\DB::get_info();
+                $sqltime  = round($bdinfo[0], 3) * 1000;
+                $sqlcount = $bdinfo[2];
+                $clear_log_timer = $log_timer * 1000 - $sqltime;
+
+                echo "<style>
+            .MBCMS-infotable.open
+            {
+                height: 99%;
+                width: 98%;
+                padding: 0;
+                padding-left: 2%;
+                padding-top: 1%;
+                opacity: 1;
+                overflow: auto;
+            }
+            
+            .MBCMS-infotable
+            {
+                z-index: 1000; 
+                height: 20px;
+                position: fixed; 
+                background: #ffad4f; 
+                padding: 5px 20px;
+                bottom: 0; 
+                left: 0;
+                opacity: 0.5;
+                width: 350px;
+                cursor: pointer;
+                border-top-right-radius: 10px;
+            }
+            
+            .MBCMS-infotable pre
+            {
+                border-top: 1px solid #eee;
+                padding-top: 20px;
+            }
+            
+            .MBCMS-infotable:hover
+            {
+                opacity: 1;
+            }
+            
+            body.help-content-close
+            {
+                overflow: hidden;
+            }
+</style>";
+
+                echo '
+                    <div class="MBCMS-infotable">
+                    mods: ' . \Module::get_modules_connect_count() . ' time: ' . ($log_timer * 1000). "({$clear_log_timer})" . 'ms; SQL ' . $sqlcount . ' за ' . $sqltime . 'ms
+                    <pre>
+                        '. $bdinfo[1] .'
+                    </pre>
+                    </div>
+                    ';
+
+
+                echo "
+                <script>
+                    $('.MBCMS-infotable')
+                    .click(function()
+                    {
+                        $(this).toggleClass('open'); 
+                        $('body').toggleClass('help-content-close');
+                    });
+                
+                </script>
+                ";
+            }
+        }
+    }
 
     public function init()
     {
@@ -25,7 +129,6 @@ class Site extends \Module
         $this->view_prioritet_index(-10);
 
         $resultModule = routes::start();
-
 
 
         $wp = new wrapper();
@@ -52,13 +155,12 @@ class Site extends \Module
 
         event::factory(new load())->call();
 
-
         if (!routes::is_admin())
         {
             $this->json = json_encode([
                 'is_static_templates' => configuration::factory()->is_static_templates(),
                 'idTemplate'          => routes::get_current_idTemplate(),
-                'is_admin'            => configuration::factory()->is_superadmin(),
+                'is_admin'            => (boolean) configuration::factory()->is_superadmin(),
             ]);
         }
 
