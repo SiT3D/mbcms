4de7f6e9eead788ae2e998cd877bcded05b3eadb
SiT3D <sit3d@yandex.ru>
2017-07-03 16:40:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * ROOT DIR . DIRECTORY_SEPARATOR
  */
@@ -21,20 +22,4 @@ foreach (\Modules::get_main_views() as $view)
 ////////////// лог посещаемости и скорости работы сайта. //////////////////
 
 $log_timer = microtime_float($ttttttttttt, 'time', false);
-$log_timer = round($log_timer, 3);
-
-if (!\MBCMS\routes::is_admin() && !\MBCMS\routes::is_ajax())
-{
-    logger::write('people', [
-        'browser_time_to_ms' => $log_timer * 1000,
-        'page'               => $_SERVER['REQUEST_URI'],
-        'ip'                 => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '',
-    ]);
-
-    if (\MBCMS\configuration::factory()->is_dev_mod() && \trud\classes\auth::factory()->admin())
-    {
-        echo '<div style="z-index: 1000; position: fixed; background: #ffad4f; padding: 5px 20px; bottom: 0; left: 0; opacity: 0.5; pointer-events: none">
-    mods: ' . Module::get_modules_connect_count() . ' time: '. ($log_timer * 1000) . 'ms</div>';
-    }
-}
-
+\MBCMS\Site::site_info_panel($log_timer);
